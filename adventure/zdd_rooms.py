1bee307d3b27203b1947d13b3a0590f229290498
Marcel-H1 <marcel.hilgers1@gmail.com>
2024-11-18 14:57:44 +0100

    Fixed spelling mistakes and added additional options

--- adventure/zdd_rooms.py
+++ adventure/zdd_rooms.py
@@ -11,38 +11,47 @@ class ToiletCellar(Room):
             # Remove book from inventory
             return [x for x in user_items if x.name != "old book"]
         return user_items
-    
-    
+
+
 class MireviLab(Room):
     def __init__(self, name, description, items=None):
         super().__init__(name, description, items)
         self.code_found = False
         self.correct_code = "9989"
-    
+
     def run_story(self, user_items):
         print("You enter the Mirevi lab.")
-        print("You feeel weirdly light. In the Mirevi Lab the floor is a bit more bouncy.")
+        print("You feel weirdly light. In the Mirevi Lab the floor is a bit more bouncy.")
         print("Why is that?")
         print("You begin to investigate....")
-        
+
         # Hint about the code
         print("You find a piece of paper with a code on it. It says'10000 - the Building number of the ZDD'.")
         print("You wonder what that means. You decide to keep it.")
         self.code_found = True
-        
+
         print("You find a VR headset on the Table. Its tempting to put it on.")
-        action = input("Do you want to put on the VR headset? (yes/no): ").lower()
-        if action == "yes":
-            print("You put on the VR headset and suddenly find yourself in a virtual world.")
-            self.run_virtual_world()
-        else:
-            print("You decide not to put on the VR headset.")
-        return user_items
-        
+        while True:
+            action = input("Do you want to put on the VR headset? (yes/no): ").lower()
+            if action == "yes":
+                print("You put on the VR headset and suddenly find yourself in a virtual world.")
+                user_items.append("vr_headset", "A VR headset that takes you to a virtual world.", movable=True)
+                self.run_virtual_world()
+            else:
+                print("You decide not to put on the VR headset.")
+                print("Instead you look around for other clues.")
+                print("Behind a shelf you find a red button that says 'Do not press!'.")
+                action = input("Do you want to press the button? (yes/no): ").lower()
+                if action == "yes":
+                    print("You press the button. The lights flicker and you hear a click.")
+                    print("Suddenly the room fills with a weird smell. You feel strangely intigued to dance your name.")
+                    print("You have emberrased yourself in the Mirevi Lab.")
+            return user_items
+
     def run_virtual_world(self):
         print("You find yourself in a virtual world. After looking around you find something that looks like... a door?")
-        print("You walk towards it and but it's locked. Nex to it is a Keypad.")
-        
+        print("You walk towards it but it's locked. Next to it is a Keypad.")
+
         # Check if the user has the code
         if self.code_found:
             attempt = input("You decide to try a code. What code do you want to try?: ")
@@ -57,9 +66,9 @@ class MireviLab(Room):
         else:
             print("You don't have the code to unlock the door. You decide to take off the VR headset.")
             print("You decide to investigate once more.")
-        
-        
-        
+
+
+
 
 ## ----------------------------------------------------------------
 ## List here all rooms
